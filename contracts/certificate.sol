88ed5c9735360bf709b30dc54ee5e58ecce17d51
angusclark1 <105189858+angusclark1@users.noreply.github.com>
2022-10-20 17:05:52 +1000

    Add files via upload
    Solidity contract for a certificate

---
+++ contracts/certificate.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+
+contract Certificate is ERC721Full {
+    constructor() public ERC721Full("Authentic Item", "AUTH") {}
+
+    function awardCertificate(address buyer, string memory description)
+        public
+        returns (uint256)
+    {
+        uint256 newCertificateId = totalSupply();
+        _mint(buyer, newCertificateId);
+        _setTokenURI(newCertificateId, description);
+
+        return newCertificateId;
+    }
+}
